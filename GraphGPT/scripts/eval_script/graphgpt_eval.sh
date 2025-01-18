8f69ab978d262d2e017425a8bfa8500ec6de8b9a
noare <zheh246@gmail.com>
2025-01-18 19:54:53 +0800

    fine tune natural languaged graph, graph-as-prefix, 2k rtlcoder gpt4, w/o + w module head

--- GraphGPT/scripts/eval_script/graphgpt_eval.sh
+++ GraphGPT/scripts/eval_script/graphgpt_eval.sh
@@ -1,9 +1,9 @@
 # to fill in the following path to extract projector for the second tuning stage!
-output_model=/data/LPJ/ICML25/all_checkpoints/train_freeze_gnn_with_eval_dataset/without_module_head/v1_lr2e4_100epoch_2batch/model_epoch=92-step=837.ckpt
+output_model=/data/LPJ/ICML25/all_checkpoints/train_freeze_gnn_with_eval_dataset/with_module_head/v0_lr5e4_100epoch_batch2/lr5e4_100epoch_batch2.ckpt
 tokenizer_path=/data/LPJ/new_CodeLlama-7b-Instruct-hf
-datapath=/data/LPJ/ICML25/graphgpt_dataset/HiVerilog_Eval/availiabe_for_graphcoder/conversations.json
-graph_data_path=/data/LPJ/ICML25/graphgpt_dataset/HiVerilog_Eval/availiabe_for_graphcoder/graph.jsonl
-res_path=/data/LPJ/ICML25/GraphCoder/HiVerilog_eval_result/train_freeze_gnn_with_eval_dataset/without_module_head/v2_lr2e4_100epoch_2batch
+datapath=/data/LPJ/ICML25/GraphCoder/graphgpt_dataset/HiVerilog_Eval/graph_as_prefix/with_module_head/availiable_for_graphcoder/conversations.json
+graph_data_path=/data/LPJ/ICML25/GraphCoder/graphgpt_dataset/HiVerilog_Eval/graph_as_prefix/with_module_head/availiable_for_graphcoder/graph_output.jsonl
+res_path=/data/LPJ/ICML25/GraphCoder/HiVerilog_eval_result/train_freeze_gnn_with_eval_dataset/with_module_head/v2_lr5e4_100epoch_2batch
 num_gpus=4
 bert_path='/data/LPJ/bert/bert-L12-H128-uncased'
 bert_tokenizer_max_length=25
@@ -27,4 +27,4 @@ python ./graphgpt/eval/run_graphgpt.py \
     --graph_data_path ${graph_data_path} \
     --output_res_path ${res_path} \
     --num_gpus ${num_gpus}\
-    --n_pass_k ${n_pass_k}
+    --n_pass_k ${n_pass_k}\
