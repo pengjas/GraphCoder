dda92ba4c4acc68214294548b171cc560c2eac2c
noare <zheh246@gmail.com>
2025-01-20 12:57:30 +0800

    debug eval

--- GraphGPT/scripts/eval_script/graphgpt_eval.sh
+++ GraphGPT/scripts/eval_script/graphgpt_eval.sh
@@ -1,9 +1,9 @@
 # to fill in the following path to extract projector for the second tuning stage!
-output_model=/data/LPJ/ICML25/all_checkpoints/train_freeze_gnn_with_eval_dataset/with_module_head/v0_lr5e4_100epoch_batch2/lr5e4_100epoch_batch2.ckpt
+output_model=/data/LPJ/ICML25/all_checkpoints/train_unfreeze_gnn_with_eval_dataset/with_module_head/v2_lr3e1_70epoch_batch2/lr3e1_70epoch_batch2_unfreeze_gnn.ckpt
 tokenizer_path=/data/LPJ/new_CodeLlama-7b-Instruct-hf
 datapath=/data/LPJ/ICML25/GraphCoder/graphgpt_dataset/HiVerilog_Eval/graph_as_prefix/with_module_head/availiable_for_graphcoder/conversations.json
 graph_data_path=/data/LPJ/ICML25/GraphCoder/graphgpt_dataset/HiVerilog_Eval/graph_as_prefix/with_module_head/availiable_for_graphcoder/graph.jsonl
-res_path=/data/LPJ/ICML25/GraphCoder/HiVerilog_eval_result/train_freeze_gnn_with_eval_dataset/with_module_head/v2_lr5e4_100epoch_2batch
+res_path=/data/LPJ/ICML25/GraphCoder/HiVerilog_eval_result/train_unfreeze_gnn_with_eval_dataset_tune_projector_lora/with_module_head/v2_lr3e1_70epoch_2batch
 num_gpus=4
 bert_path='/data/LPJ/bert/bert-L12-H128-uncased'
 bert_tokenizer_max_length=25
