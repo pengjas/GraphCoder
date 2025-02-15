2f89b30a1464722a5c830566649e6ba155953add
noare <pliuas@connect.ust.hk>
2025-02-15 11:20:24 +0800

    pass_k n=15

--- HiVerilog_eval_generated_code/fine_tune_havenllama_using_rtlcoder_gpt4_unfreeze_gnn_projector_with_lora/v0_best_ckpt_separate_lr_gnn8e3_projector3e4_lora3e5_batch2_epoch5/test_1/stagepipe3.v
+++
@@ -1,9 +0,0 @@
-module stagepipe3(
-    input clk,
-    input rst,
-    input [31:0] instr_mem [0:31],
-    input [31:0] reg_file [0:31],
-    output [31:0] out_reg_file  
-);
-UNABLE_TO_IMPLEMENT
-endmodule
