be540ea19cb8e4c9d2980896b906f2183e931454
noare <zheh246@gmail.com>
2025-02-27 12:27:47 +0800

    similar logic instr_reg epoch12

--- GraphGPT/scripts/eval_script/graphgpt_eval.sh
+++ GraphGPT/scripts/eval_script/graphgpt_eval.sh
@@ -1,9 +1,9 @@
 # to fill in the following path to extract projector for the second tuning stage!
-output_model=/data/LPJ/ICML25/all_checkpoints/fine_tuning_5layers_havenllama_similar_logic_with_module_head/instr_reg/v0/epoch6/haven_llama_5layers_similar_logic_instr_reg_v0_epoch6.ckpt
+output_model=/data/LPJ/ICML25/all_checkpoints/fine_tuning_5layers_havenllama_similar_logic_with_module_head/instr_reg/v0/epoch12/haven_llama_5layers_similar_logic_instr_reg_v0_epoch12.ckpt
 tokenizer_path=/data/LPJ/haven_codellama
 datapath=/data/LPJ/ICML25/GraphCoder/graphgpt_dataset/HiVerilog_Eval/specific_task/instr_reg/conversation.json
 graph_data_path=/data/LPJ/ICML25/GraphCoder/graphgpt_dataset/HiVerilog_Eval/specific_task/instr_reg/graph.jsonl
-res_path=/data/LPJ/ICML25/GraphCoder/eval_result/HiVerilog_eval_result/fine_tune_5layers_similar_logic_with_head/instr_reg/tmp0.2/v0_epoch6
+res_path=/data/LPJ/ICML25/GraphCoder/eval_result/HiVerilog_eval_result/fine_tune_5layers_similar_logic_with_head/instr_reg/tmp0.2/v0_epoch12
 num_gpus=1
 bert_path='/data/LPJ/bert/bert-L12-H128-uncased'
 bert_tokenizer_max_length=25
--- GraphGPT/scripts/tune_script_light/graphcoder_fine_tuning.sh
+++ GraphGPT/scripts/tune_script_light/graphcoder_fine_tuning.sh
@@ -6,11 +6,11 @@ instruct_ds=/data/LPJ/ICML25/GraphCoder/graphgpt_dataset/gpt_dataset_constructio
 # graph_data_path=/data/LPJ/ICML25/GraphCoder/graphgpt_dataset/train_with_eval_dataset/with_module_head/graph_as_prefix/availiable_for_graphcoder/graph_output.jsonl
 graph_data_path=/data/LPJ/ICML25/GraphCoder/graphgpt_dataset/gpt_dataset_construction/similar_logic/instr_reg/v0/with_head/graph.jsonl
 pretra_gnn=clip_gt_arxiv
-output_model=/data/LPJ/ICML25/all_checkpoints/fine_tuning_5layers_havenllama_similar_logic_with_module_head/instr_reg/v0/epoch3
+output_model=/data/LPJ/ICML25/all_checkpoints/fine_tuning_5layers_havenllama_similar_logic_with_module_head/instr_reg/v0/epoch18
 bert_path=/data/LPJ/bert/bert-L12-H128-uncased
-model_save_name=haven_llama_5layers_similar_logic_instr_reg_v0_epoch3
-resume='/data/LPJ/ICML25/all_checkpoints/fine_tuning_5layers_havenllama_gnn_projector_with_lora_using_shuffled_acl25_gpt4_with_module_head/resume_rank64_separate_lr_gnn1e4_projector1e4_lora5e5_batch2_epoch20/haven_llama_5layers_shuffled_acl25_gpt4_with_module_head_resume_rank64_separate_gnn1e4_projector1e4_lora5e5_batch2_epoch20.ckpt'
-if_resume=False
+model_save_name=haven_llama_5layers_similar_logic_instr_reg_v0_epoch18
+resume='/data/LPJ/ICML25/all_checkpoints/fine_tuning_5layers_havenllama_similar_logic_with_module_head/instr_reg/v0/epoch12/haven_llama_5layers_similar_logic_instr_reg_v0_epoch12.ckpt'
+if_resume=True
 val_data_path=/data/LPJ/ICML25/GraphCoder/graphgpt_dataset/train_with_eval_dataset/with_module_head/graph_as_prefix/availiable_for_graphcoder/conversations.json
 val_graph_data_path=/data/LPJ/ICML25/GraphCoder/graphgpt_dataset/train_with_eval_dataset/with_module_head/graph_as_prefix/availiable_for_graphcoder/graph_output.jsonl
 val_early_stop_threshold=0.1
@@ -31,7 +31,7 @@ python graphgpt/train/train_light.py \
     --use_graph_start_end True \
     --bf16 True \
     --output_dir ${output_model} \
-    --num_train_epochs 3 \
+    --num_train_epochs 18 \
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 1 \
     --real_batch_size 2 \
