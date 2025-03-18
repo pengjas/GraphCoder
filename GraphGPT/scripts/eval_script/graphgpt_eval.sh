2782b2dc759aae40c5964ca7be60b096c4eb9e36
noare <zheh246@gmail.com>
2025-03-18 11:07:35 +0800

    verification for HiVerilog expansion

--- GraphGPT/scripts/eval_script/graphgpt_eval.sh
+++ GraphGPT/scripts/eval_script/graphgpt_eval.sh
@@ -1,10 +1,10 @@
 # to fill in the following path to extract projector for the second tuning stage!
 output_model=/data/LPJ/ICML25/all_checkpoints/pretrain_gnn_qformer_havenllama_using_1989_57_without_lora/v0_50epoch_separate_lr_gnn1e3_qformer_5e4/haven_llama_qformer_1989_57_pretrain_without_lora_50epoch_separate_lr_gnn1e3_qformer_5e4.ckpt
 tokenizer_path=/data/LPJ/haven_codellama
-datapath=/data/LPJ/ICML25/GraphCoder/graphgpt_dataset/training_dataset_eval/stage2_1989_57_eval/eval/conversations.json
-graph_data_path=/data/LPJ/ICML25/GraphCoder/graphgpt_dataset/training_dataset_eval/stage2_1989_57_eval/eval/graph.jsonl
-res_path=/data/LPJ/ICML25/GraphCoder/eval_result/training_dataset_eval_result/stage2_1989_57/qformer_50epoch_separate_lr_gnn1e3_qformer_5e4/tmp0.2
-num_gpus=1
+datapath=/data/LPJ/ICML25/GraphCoder/graphgpt_dataset/HiVerilog_expansion_eval/with_head/conversations.json
+graph_data_path=/data/LPJ/ICML25/GraphCoder/graphgpt_dataset/HiVerilog_expansion_eval/with_head/graph.jsonl
+res_path=/data/LPJ/ICML25/GraphCoder/eval_result/HiVerilog_eval_result/pretrain_gnn_qformer_havenllama_using_1989_57_without_lora/v0_50epoch_separate_lr_gnn1e3_qformer_5e4/tmp0.2
+num_gpus=4
 bert_path='/data/LPJ/bert/bert-L12-H128-uncased'
 bert_tokenizer_max_length=25
 # conv_mode=qwen
@@ -13,7 +13,7 @@ bf16=True
 f16=False
 output_file_name='eval_res'
 model_max_length=9216
-n_pass_k=1
+n_pass_k=15
 use_trained_gnn=True
 lora_enable=False
 lora_r=64
