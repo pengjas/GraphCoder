fe1f45379676903e707f84ee2f728c419c3a51df
noare <zheh246@gmail.com>
2025-04-17 18:13:22 +0800

    gpt4o upon hiverilog tmp=0.2

--- GraphGPT/scripts/eval_script/graphgpt_eval.sh
+++ GraphGPT/scripts/eval_script/graphgpt_eval.sh
@@ -1,22 +1,22 @@
 # to fill in the following path to extract projector for the second tuning stage!
-output_model=/data/LPJ/ICML25/all_checkpoints/fine_tune_qformer_havenllama_using_1989_57_with_lora/v5_cleaned_graph_from_resume_v4_6epoch_separate_lr_gnn5e4_qformer5e5_lora8e6_rank64/fine_tune_qformer_havenllama_using_1989_57_with_lora_v5_cleaned_graph_from_resume_v4_6epoch_separate_lr_gnn5e4_qformer5e5_lora8e6_rank64.ckpt
-tokenizer_path=/data/LPJ/haven_codellama
+output_model=/data/LPJ/ICML25/all_checkpoints/pretrain_qformer_havenqwen_using_1989_57_without_lora/v1_cleaned_graph_20epoch_separate_lr_gnn2e3_qformer5e4/pretrain_qformer_havenqwen_using_1989_57_without_lora_v1_cleaned_graph_20epoch_separate_lr_gnn2e3_qformer5e4.ckpt
+tokenizer_path=/data/LPJ/haven_qwen
 datapath=/data/LPJ/ICML25/GraphCoder/graphgpt_dataset/HiVerilog_expansion_eval/with_head/clean_graph/conversations.json
 graph_data_path=/data/LPJ/ICML25/GraphCoder/graphgpt_dataset/HiVerilog_expansion_eval/with_head/clean_graph/graph.jsonl
-res_path=/data/LPJ/ICML25/GraphCoder/eval_result/HiVerilog_expansion_result/fine_tune_qformer_havenllama_using_1989_57_with_lora/v5_cleaned_graph_from_resume_6epoch_separate_lr_gnn5e4_qformer5e5_lora8e6_rank64/tmp0.2
+res_path=/data/LPJ/ICML25/GraphCoder/eval_result/HiVerilog_expansion_result/pretrain_qformer_havenqwen_using_1989_57_without_lora/v1_cleaned_graph_20epoch_separate_lr_gnn2e3_qformer5e4/tmp0.2
 num_gpus=4
 bert_path='/data/LPJ/bert/bert-L12-H128-uncased'
 bert_tokenizer_max_length=25
-# conv_mode=qwen
-conv_mode=graphchat_v1
+conv_mode=qwen
+# conv_mode=graphchat_v1
 bf16=True
 f16=False
 output_file_name='eval_res'
 # model_max_length=9216
 model_max_length=6912
-n_pass_k=15
+n_pass_k=10
 use_trained_gnn=True
-lora_enable=True
+lora_enable=False
 lora_r=64
 lora_alpha=128
 temperature=0.2
@@ -52,47 +52,3 @@ python ./graphgpt/eval/run_graphgpt.py \
 
 
 
-
-model=/data/LPJ/ICML25/llamafactory/fine_tuned_model/haven_llama_stage2_1989_57/v2_without_graph_3peoch
-output_file=eval_res
-output_dir=/data/LPJ/ICML25/llamafactory/inference_rslt/havenllama_3epochs_hiverilog_expansion/v2_without_graph_max_length_6912/tmp0.2
-benchmark_jsonl=/data/LPJ/ICML25/GraphCoder/graphgpt_dataset/HiVerilog_expansion_eval/for_text_llm/clean_graph/conversations.jsonl
-temperature=0.2
-gpu_name=4
-n=15
-tokenizer=/data/LPJ/haven_codellama
-max_length=6912
-
-python /data/LPJ/ICML25/llamafactory/model_inference/inference_ray.py \
-    --model ${model} \
-    --output_file ${output_file} \
-    --output_dir ${output_dir} \
-    --benchmark_jsonl ${benchmark_jsonl} \
-    --temperature ${temperature} \
-    --gpu_name ${gpu_name} \
-    --n ${n} \
-    --tokenizer ${tokenizer} \
-    --max_length ${max_length}
-
-
-
-model=/data/LPJ/ICML25/llamafactory/fine_tuned_model/haven_llama_stage2_1989_57/v3_clean_graph_9peoch
-output_file=eval_res
-output_dir=/data/LPJ/ICML25/llamafactory/inference_rslt/havenllama_3epochs_hiverilog_expansion/v3_clean_graph_max_length_6912_9epochs/tmp0.2
-benchmark_jsonl=/data/LPJ/ICML25/GraphCoder/graphgpt_dataset/HiVerilog_expansion_eval/for_text_llm/clean_graph/conversations.jsonl
-temperature=0.2
-gpu_name=4
-n=15
-tokenizer=/data/LPJ/haven_codellama
-max_length=6912
-
-python /data/LPJ/ICML25/llamafactory/model_inference/inference_ray.py \
-    --model ${model} \
-    --output_file ${output_file} \
-    --output_dir ${output_dir} \
-    --benchmark_jsonl ${benchmark_jsonl} \
-    --temperature ${temperature} \
-    --gpu_name ${gpu_name} \
-    --n ${n} \
-    --tokenizer ${tokenizer} \
-    --max_length ${max_length}
